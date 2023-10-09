0f703c64468682f68f5343dc92db8b6c1846e014
rafa <42rpaulino@gmail.com>
2023-10-09 15:08:46 -0300

    adding more quality life stuff

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -10,17 +10,19 @@
 
 <div class="verticalWall"	style="top: {20}px;		left: {leftShift + 390}px;"></div>
 
-<div class="score"			style="top: {60}px;		left: {leftShift + 250}px;">0</div>
-<div class="score"			style="top: {60}px;		left: {leftShift + 460}px;">0</div>
+<div class="score"			style="top: {60}px;		left: {leftShift + 250}px;">{scoreA}</div>
+<div class="score"			style="top: {60}px;		left: {leftShift + 460}px;">{scoreB}</div>
 
 <div class="info"			style="top: {650}px;	left: {leftShift + 0}px;">800x600px object width 20px, paddle 100px</div>
-<div class="info"			style="top: {650}px;	left: {leftShift + 900}px;">{paddleBx} , {paddleBy}</div>
+<div class="info"			style="top: {650}px;	left: {leftShift + 900}px;">{ball.velocity}</div>
 <!-- <div>
 	<Socket />
 </div> -->
 
 <script lang="ts">
 	let leftShift = 400;
+	let scoreA = 0;
+	let scoreB = 0;
 	let paddleAx = leftShift + 35;
 	let paddleBx = leftShift + 745;
 	let paddleAy = 20 + 300 - 50;
@@ -46,27 +48,47 @@
 		ball.y += ball.velocity * Math.sin(ball.radians);
 		if (ball.x < leftShift || ball.x > leftShift + 800) {
 		// Reflect horizontally
-			ball.x = resetx;
+			if(ball.x < leftShift) {
+				scoreB += 1;
+				ball.x = leftShift + 735;
+			}
+			else {
+				scoreA += 1;
+				ball.x = leftShift + 45;
+			}
 			ball.y = resety;
+			ball.velocity = 2.5;
 		}
 
 		if ( (ball.y > paddleAy && ball.y < paddleAy + paddleSize ) 
-		&& (ball.x > paddleAx - 10 && ball.x < paddleAx + 10))
+		&& (ball.x > paddleAx - 10 && ball.x < paddleAx + 10)) {
 			ball.radians = Math.PI - ball.radians;
+			ball.velocity += 0.25;
+			if(ball.velocity < 5)
+				ball.velocity += 0.25;
+		}
 	
 		if ( (ball.y > paddleBy && ball.y < paddleBy + paddleSize ) 
-		&& (ball.x > paddleBx - 10 && ball.x < paddleBx + 10))
+		&& (ball.x > paddleBx - 10 && ball.x < paddleBx + 10)) {
 			ball.radians = Math.PI - ball.radians;
+			if(ball.velocity < 5)
+				ball.velocity += 0.25;
+		}
 
 		if (ball.y <= topWall || ball.y >= botWall) {
 			ball.radians = -ball.radians;
 			console.log("HIT X");
 		}
-		//TO DO, add hitbox upper e lower paddle, more angles, change velocity at hit, change ball startpoint and reset, add score
+		//TO DO, add hitbox upper e lower paddle, more on hit pedal angles
 	}
 	async function gameloop() {
 		while(1) {
+			if(scoreA > 10 || scoreB > 10) {
+				scoreA = 0;
+				scoreB = 0;
+			}
 			newBallPosition();
+			paddleBy = ball.y - 50; //BOT IMPLEMENTATION
 			await sleep(1);
 		}
 	}
@@ -81,12 +103,12 @@
 			keyDownInterval = setInterval(() => {
 				if (direction === 'up' && paddleAy >= 30) {
 					paddleAy -= 10; // Move rectangle 1 upward
-					paddleBy -= 10;
+					// paddleBy -= 10;
 				} else if (direction === 'down' && paddleAy < 520) {
 					paddleAy += 10; // Move rectangle 2 downward
-					paddleBy += 10;
+					// paddleBy += 10;
 				}
-			}, 20); // Adjust the interval as needed for desired speed
+			}, 15); // Adjust the interval as needed for desired speed
 		}
 	}
 
